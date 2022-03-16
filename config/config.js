2731e335b10b83ec9b8b36ad6e48a67861654dd7
vincentvanft <99083041+vincentvanft@users.noreply.github.com>
2022-03-16 22:36:41 +0800

    Fixed everything

---
+++ config/config.js
@@ -0,0 +1,20 @@
+const config = {
+    app_name: 'CRK Rarity Ranking',
+    app_description: 'Check the rarity of Crypto Rich Kids NFTs.',
+    collection_file_name: 'collection.json',
+    collection_contract_address: '0xfaC7fe5e11FcbC837c2E115626324CAAf3A56dD9',
+    collection_name: 'Crypto Rich Kids',
+    collection_description: 'An elite collection of NFTs highlighting the transfer of wealth from the traditional “boomer” markets to the crypto youth of the world.',
+    collection_id_from: 1,
+    ignore_traits: ['date'], 
+    sqlite_file_name: 'database.sqlite',
+    ga: 'G-BW69Z04YTP',
+    main_og_image: 'https://crkograrity.herokuapp.com/images/og.png',
+    item_path_name: 'CRK',
+    page_item_num: 10,
+    content_image_is_video: false,
+    content_image_frame: 'rectangle', // circle, rectangle
+    use_wallet: false
+};
+
+module.exports = config;
